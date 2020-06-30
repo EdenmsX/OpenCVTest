8d63aa201444a810d064645438fd7621982749b2
liulibin <liulibin@brilliance.com.cn>
2020-06-30 22:48:46 +0800

    add colorSpaceReduce1

--- OpenCVDemo_Mac/OpenCVDemo_Mac/main.cpp
+++ OpenCVDemo_Mac/OpenCVDemo_Mac/main.cpp
@@ -58,6 +58,99 @@ void drawCircle(Mat &mat_image, Point2i center, double radius){
 }
 
 
+/// 指针访问
+/// @param mat_image_in  输入图片
+/// @param mat_image_out  输出图片
+/// @param div 缩减级别
+void colorSpaceReduce1(Mat &mat_image_in, Mat &mat_image_out, int div) {
+    //拷贝图片
+    mat_image_out = mat_image_in.clone();
+    int rows = mat_image_out.rows;
+    int cols = mat_image_out.cols;
+    
+    //通道数量(每一个像素点有多少个颜色)
+    int channels = mat_image_out.channels();
+    //每一行的元素个数
+    int colsSize = cols * channels;
+    
+    //循环缩减颜色通道
+    for (int i = 0; i< rows; i++) {
+        //获取第i行(通过指针访问像素点)
+        uchar *data = mat_image_out.ptr<uchar>(i);
+        for (int j = 0 ; j< colsSize; j++) {
+            //指针位移
+            //data[j]表示一个颜色值(可能是R, G ,B中的任何一个)
+            /**
+             缩减公式根据实际需求自定义
+             eg. data[j] = 100   div = 24
+             data[j] = 100 / 24 * 24 = 96    (100 / 24 =  4)
+             */
+            data[j] = data[j] / div * div;
+        }
+        
+    }
+}
+
+/// 迭代器访问
+/// @param mat_image_in  输入图片
+/// @param mat_image_out  输出图片
+/// @param div 缩减级别
+void colorSpaceReduce2(Mat &mat_image_in, Mat &mat_image_out, int div) {
+    mat_image_out = mat_image_in.clone();
+    //迭代器
+    Mat_<Vec3b>::iterator it_start = mat_image_out.begin<Vec3b>();
+    Mat_<Vec3b>::iterator it_end = mat_image_out.end<Vec3b>();
+    
+    //循环缩减颜色通道
+    for (; it_start != it_end; it_start++) {
+        //(*it_start): 表示获取像素点
+        //(*it_start)[0]: 表示获取像素点值
+        (*it_start)[0] = (*it_start)[0] / div * div; //B
+        (*it_start)[1] = (*it_start)[1] / div * div; //G
+        (*it_start)[2] = (*it_start)[2] / div * div; //R
+    }
+}
+
+
+/// 动态地址计算
+/// @param mat_image_in  输入图片
+/// @param mat_image_out  输出图片
+/// @param div 缩减级别
+void colorSpaceReduce3(Mat &mat_image_in, Mat &mat_image_out, int div) {
+    mat_image_out = mat_image_in.clone();
+    int rows = mat_image_out.rows;
+    int cols = mat_image_out.cols;
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            mat_image_out.at<Vec3b>(i, j)[0] = mat_image_out.at<Vec3b>(i, j)[0] / div * div;
+            mat_image_out.at<Vec3b>(i, j)[1] = mat_image_out.at<Vec3b>(i, j)[1] / div * div;
+            mat_image_out.at<Vec3b>(i, j)[2] = mat_image_out.at<Vec3b>(i, j)[2] / div * div;
+        }
+    }
+}
+
+
+
+void roiDemo(){
+    //第一步：创建一张图片,空白图片
+    Mat mat_image_src = imread("/Users/liulibin/Desktop/image1.png");
+    Mat mat_image_logo = imread("/Users/liulibin/Desktop/image2.jpeg");
+    
+    //第二步：灰度处理,加载一张灰度图片
+    Mat mat_image_mask = imread("/Users/liulibin/Desktop/image2.jpeg");
+    
+    //第三步：定义ROI区域
+    //根据原始图片,得到src图片一块内存地址(指针)
+    Mat mat_image_roi = mat_image_src(Rect2i(0, 0, mat_image_logo.cols, mat_image_logo.rows));
+    
+    //第三步：拷贝到ROI区域
+    //参数一：ROI区域
+    //参数二：需要填充图片(水印)
+    mat_image_logo.copyTo(mat_image_roi, mat_image_mask);
+}
+
+
 
 int main( int argc, char** argv ){
 //    string imageName("/Users/liulibin/Desktop/12.png");
@@ -76,11 +169,18 @@ int main( int argc, char** argv ){
     //绘制椭圆
 //    drawEllipse(image, 0);
     //绘制圆
-    drawCircle(image, Point2i(WINDOWS_WIDTH/2, WINDOWS_WIDTH/2), WINDOWS_WIDTH/4);
+//    drawCircle(image, Point2i(WINDOWS_WIDTH/2, WINDOWS_WIDTH/2), WINDOWS_WIDTH/4);
     
+    Mat mat_image_src = imread("/Users/liulibin/Desktop/12.png");
+    //访问图像像素
+    //1. 指针访问
+    Mat mat_image_dst;
+    mat_image_dst.create(mat_image_src.rows, mat_image_src.cols, mat_image_src.type());
     
-    namedWindow( "Display window", WINDOW_AUTOSIZE );
-    imshow( "Display window", image );
+    colorSpaceReduce3(mat_image_src, mat_image_dst, 24);
+    
+    namedWindow( "Display window" );
+    imshow( "Display window", mat_image_dst );
     waitKey(0);
     
     return 0;
